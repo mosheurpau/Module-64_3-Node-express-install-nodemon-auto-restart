e4e5dc3d5f0e3acba7477cda28c15366b5407bf3
mosheurpau <mosheur.pau@gamil.com>
2022-04-21 16:22:23 +0600

    Create dynamic api, api parameter, access params

--- index.js
+++ index.js
@@ -6,6 +6,32 @@ app.get("/", (req, res) => {
   res.send("Hello from my personal Smarty  Pant !! with auto rester");
 });
 
+const users = [
+  { id: 1, name: "Sabana", email: "Sabana@gmail.com", phone: "0178888888" },
+  { id: 2, name: "Shabnoor", email: "Shabnoor@gmail.com", phone: "0178888888" },
+  {
+    id: 3,
+    name: "Suchorita",
+    email: "Suchorita@gmail.com",
+    phone: "0178888888",
+  },
+  { id: 4, name: "mehedi", email: "mehedi@gmail.com", phone: "0178888888" },
+  { id: 5, name: "srabonti", email: "srabonti@gmail.com", phone: "0178888888" },
+  { id: 6, name: "sabila", email: "sabila@gmail.com", phone: "0178888888" },
+  { id: 7, name: "sohana", email: "sohana@gmail.com", phone: "0178888888" },
+];
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.get("/users/:id", (req, res) => {
+  console.log(req.params);
+  const id = parseInt(req.params.id);
+  const user = users.find((u) => u.id === id);
+  res.send(user);
+});
+
 app.listen(port, () => {
   console.log("Listening to part", port);
 });
