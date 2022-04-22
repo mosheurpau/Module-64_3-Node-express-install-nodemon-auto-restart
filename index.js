bd800464f2cf8edce617c408fab26e93b02b8e62
mosheurpau <mosheur.pau@gamil.com>
2022-04-22 22:58:58 +0600

    Display POST data on the UI and explore query parameter

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello from my personal Smarty  Pant !! with auto rester");
@@ -25,13 +26,29 @@ const users = [
 ];
 
 app.get("/users", (req, res) => {
-  res.send(users);
+  if (req.query.name) {
+    const search = req.query.name.toLowerCase();
+    const matched = users.filter((user) =>
+      user.name.toLowerCase().includes(search)
+    );
+    res.send(matched);
+  } else {
+    res.send(users);
+  }
 });
 
 app.get("/users/:id", (req, res) => {
   console.log(req.params);
   const id = parseInt(req.params.id);
   const user = users.find((u) => u.id === id);
+  res.send(user);
+});
+
+app.post("/user", (req, res) => {
+  console.log("request", req.body);
+  const user = req.body;
+  user.id = users.length + 1;
+  users.push(user);
   res.send(user);
 });
 
